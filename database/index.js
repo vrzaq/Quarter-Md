0a3a6c9bb465b1c5c5ee757e5174d5f0c99d267b
Khirazz Dev <106077153+vrzaq@users.noreply.github.com>
2023-12-01 21:14:45 +0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -13,39 +13,21 @@ const dbName = "database.json";
 const file = {
   user: join(dirr, "user." + dbName),
   group: join(dirr, "group." + dbName),
-  config: join(dirr, "config." + dbName),
-  werewolf: join(dirr, "werewolf." + dbName),
+  config: join(dirr, "config." + dbName)
 };
 
 accessSync(file.user);
 accessSync(file.group);
 accessSync(file.config);
-accessSync(file.werewolf);
 
 export let db = {
   user: JSON.parse(readFileSync(file.user)),
   group: JSON.parse(readFileSync(file.group)),
-  config: JSON.parse(readFileSync(file.config)),
-  werewolf: JSON.parse(readFileSync(file.werewolf)),
-  game: {
-    rpg: {
-      dungeon: {
-        user: JSON.parse(readFileSync('./database/separate/rpg-dungeon/userRpg.json')),
-        equipment: JSON.parse(readFileSync('./database/separate/rpg-dungeon/equipmentRpg.json')),
-        login: JSON.parse(readFileSync('./database/separate/rpg-dungeon/login.json')),
-        hunt: JSON.parse(readFileSync('./database/separate/rpg-dungeon/huntRpg.json')),
-      },
-    },
-  },
-  webp: {
-    server: JSON.parse(readFileSync('./database/separate/create-webp/server.json')),
-    group: JSON.parse(readFileSync('./database/separate/create-webp/group.json')),
-  }, 
+  config: JSON.parse(readFileSync(file.config))
 };
 
 setInterval(async() => {
   writeFileSync(file.user, JSON.stringify(db.user, null, 2));
   writeFileSync(file.group, JSON.stringify(db.group, null, 2));
   writeFileSync(file.config, JSON.stringify(db.config, null, 2));
-  writeFileSync(file.werewolf, JSON.stringify(db.werewolf, null, 2));
 }, 990); 
